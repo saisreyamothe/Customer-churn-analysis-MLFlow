031d0d8a1d038003ae870e41ddbaeafd602c1f6b
saisreyamothe <saisreya9@gmail.com>
2026-06-25 21:02:10 +0530

    Add files via upload

---
+++ setup[1].py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="churn-prediction-mlops",
+    version="0.1.0",
+    description="End-to-end MLOps pipeline for churn prediction",
+    packages=find_packages(),
+)
